294a32764d8cc67dc11f42d4bb3b2149a805a37c
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-01 03:16:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    roster.each do |k, v|
+     new_hash[k] = v.sort
+  end
+
+  new_hash
+
+    end
+
+
+end
